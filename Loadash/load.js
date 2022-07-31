55d0f1be903f7c06c93d79f26da72e8b26d280a6
Akshaymonkv <70555498+Akshaymonkv@users.noreply.github.com>
2022-07-31 20:48:35 +0530

    Loadash module

---
+++ Loadash/load.js
@@ -0,0 +1,4 @@
+var _ = require('lodash');
+//loadash is an inbuilt node module tha can be used for a number of arithmetic operations
+console.log(_.random(1,100));
+//printing a random number between 1 and 100
